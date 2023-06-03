d13cd4b5a740535db0e48b9d1e5bd5c820c34938
Your name <you@example.com>
2023-06-03 09:00:22 +0530

    First version of demo code

---
+++ Demo.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import pyodbc as odbc
+from datetime import datetime
+print(odbc.drivers())
+ConnectionString=odbc.connect(
+    'Driver={ODBC Driver 17 for SQL Server};'
+    'Server=Anushree\SQLEXPRESS;'
+    'Database=AdventureWorks2019;'
+    'Trusted_Connection=yes;'
+
+
+)
+
+Query=pd.read_sql_query(
+
+    '''
+    select businessentityid,passwordhash,rowguid
+    from Person.password
+    ''',
+    ConnectionString
+)
+
+DF=pd.DataFrame(Query)
+DF.to_csv(datetime.now().strftime("%y-%m-%d_%I-%M-%S-%p")+'-Sql user password data.csv',index=False)
